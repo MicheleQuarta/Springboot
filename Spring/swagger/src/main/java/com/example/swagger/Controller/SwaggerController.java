7e85da97b2fc3a944fe38539353705395498bfcf
MicheleQuarta <160587660+MicheleQuarta@users.noreply.github.com>
2024-05-13 22:19:02 +0200

    Add files via upload

---
+++ Spring/swagger/src/main/java/com/example/swagger/Controller/SwaggerController.java
@@ -0,0 +1,20 @@
+package com.example.swagger.Controller;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class NameController {
+
+    @GetMapping("/getName")
+    public String getName(@RequestParam String name) {
+        return "Nome: " + name;
+    }
+
+    @PostMapping("/reverseName")
+    public String reverseName(@RequestParam String name) {
+        StringBuilder reversedName = new StringBuilder(name);
+        return "Nome al contrario: " + reversedName.reverse().toString();
+    }
+}
+
