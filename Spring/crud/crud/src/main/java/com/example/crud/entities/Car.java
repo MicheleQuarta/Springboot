de943eebd406ca91b477868eb833b8738a965a89
MicheleQuarta <160587660+MicheleQuarta@users.noreply.github.com>
2024-05-23 11:15:20 +0200

    Add files via upload

---
+++ Spring/crud/crud/src/main/java/com/example/crud/entities/Car.java
@@ -0,0 +1,28 @@
+package com.example.crud.entities;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "cars")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true, nullable = false)
+    private String modelName;
+
+    @Column(nullable = false)
+    private String type;
+
+}
---
+++ Spring/crud/crud/src/main/java/com/example/crud/repositories/CarRepo.java
@@ -0,0 +1,10 @@
+package com.example.crud.repositories;
+
+import co.develhope.crud.entities.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping
+public interface CarRepo extends JpaRepository<Car, Long> {
+
+}
