ff0ab39f786ce2c53f8e38254699fae386ca81ef
MicheleQuarta <160587660+MicheleQuarta@users.noreply.github.com>
2024-05-24 00:05:43 +0200

    Add files via upload

---
+++ Spring/repositories1/src/main/java/com/example/repositories1/entities/Car.java
@@ -0,0 +1,27 @@
+package com.example.repositories1.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "cars")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true, nullable = false)
+    private String modelName;
+
+    @Column(nullable = false)
+    private String serialNumber;
+
+    @Column(nullable = false)
+    private Integer currentPrice;
+}
