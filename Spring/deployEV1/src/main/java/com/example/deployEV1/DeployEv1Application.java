daa68770bd20365878ba7867f6e735f3c0e0d480
MicheleQuarta <160587660+MicheleQuarta@users.noreply.github.com>
2024-06-17 15:39:54 +0200

    Add files via upload

---
+++ Spring/deployEV1/src/main/java/com/example/deployEV1/DeployEv1Application.java
@@ -0,0 +1,13 @@
+package com.example.deployEV1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployEv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployEv1Application.class, args);
+	}
+
+}
