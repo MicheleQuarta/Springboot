6f8d40c06d93123fe81f588732645d995f84c647
MicheleQuarta <160587660+MicheleQuarta@users.noreply.github.com>
2024-06-17 15:39:02 +0200

    Add files via upload

---
+++ Spring/deploy1/src/main/java/com/example/deploy1/BasicController.java
@@ -0,0 +1,14 @@
+package com.example.deploy1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BasicController {
+
+    @GetMapping("/devName")
+    public String getDevName() {
+        return "Your Name";
+    }
+
+}
