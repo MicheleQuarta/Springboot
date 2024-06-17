e24274a35d5bdac5ffaeb2a2000d8f755b051777
MicheleQuarta <160587660+MicheleQuarta@users.noreply.github.com>
2024-06-17 15:40:19 +0200

    Add files via upload

---
+++ Spring/deployEV2/src/main/java/com/example/deployEV2/controllers/WelcomeController.java
@@ -0,0 +1,18 @@
+package com.example.deployEV2.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @Value("${welcomeMsg}")
+    private String welcomeMsg;
+
+    @GetMapping("/")
+    public String welcome() {
+        return welcomeMsg;
+    }
+
+}
---
+++ Spring/deployEV2/src/test/java/com/example/deployEV2/DeployEv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deployEV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployEv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
