c31712c465ff55817199cc35e5175215dd5c16a1
MicheleQuarta <160587660+MicheleQuarta@users.noreply.github.com>
2024-06-24 18:57:48 +0200

    Add files via upload

---
+++ Spring/deployEV2/src/main/java/com/example/deployEV2/controllers/LoggingService/src/main/java/com/example/LoggingService/controllers/BasicController.java
@@ -0,0 +1,40 @@
+package com.example.LoggingService.controllers;
+
+import com.example.LoggingService.services.CalculationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BasicController {
+
+    private static final Logger logger = LoggerFactory.getLogger(BasicController.class);
+
+    @Autowired
+    private CalculationService calculationService;
+
+    @GetMapping("/")
+    public String welcome() {
+        logger.info("Welcome message logged.");
+        return "Welcome to my Spring Boot application!";
+    }
+
+    @GetMapping("/exp")
+    public String calculatePower() {
+        logger.debug("Starting calculation...");
+        double result = calculationService.calculatePower();
+        logger.debug("Calculation completed. Result: {}", result);
+        return "The power is: " + result;
+    }
+
+    @GetMapping("/get-errors")
+    public ResponseEntity<String> throwCustomError() {
+        logger.error("An error occurred!");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Custom error occurred!");
+    }
+
+}
